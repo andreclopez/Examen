3f1e0126095e89e8fd9018c8e0c0404a0dafeee9
Andrea <andhy.lopez.090@gmail.com>
2024-03-27 23:42:45 +0000

    nuevos cambios

---
+++ examen.js
@@ -0,0 +1,46 @@
+console.log("Examen")
+
+// Teniendo una variable numérica que representa el número de lados de una
+// figura geométrica, mostrar por consola su tipo. Los tipos son: "Círculo", "Triángulo", "Cuadrado", "Pentágono", "Hexágono" o "Polígono", según corresponda:
+// Círculo: si el número de lados es 0.
+// Triángulo: si el número de lados es 3.
+// Cuadrado: si el número de lados es 4.
+// Pentágono: si el número de lados es 5.
+// Hexágono: si el número de lados es 6.
+// Polígono: si el número de lados es mayor que 6.
+// Asegúrate de incluir un mensaje de error si el número de lados es menor
+// o igual a cero.
+
+console.log("Examen")
+
+function tipoFigura(lados){
+
+    if(lados == 0){
+        return "Círculo"
+    } 
+    
+    if(lados == 3){
+        return "Triángulo"
+    } 
+    
+    if(lados == 4){
+        return "Cuadrado"
+    } 
+    
+    if(lados == 5){
+        return "Pentágono"
+    } 
+    
+    if(lados == 6){
+        return "Hexágonos"
+
+    } if(lados > 6){
+        
+        return "Polígono"
+    }
+        
+}
+
+let lados = prompt("Ingrese cantidad de lados")
+
+console.log(tipoFigura(lados))
